48cef187c77095cfc23fc39059eddda41d75753a
DESKTOP-GCE894B\User <michaellopez2230@hotmail.com>
2025-11-06 08:12:35 -0500

    Limpieza de Repositorio

--- Paginaweb/Paginaweb/dom.js
+++
@@ -1,7 +0,0 @@
-const products= document.getElementById("productos");
-console.log(products);
-products.classList.add("nueva");
-//products.innerHTML="<p> hola mundo </p";
-let element = document.createElement("p");
-element.textContent = "Nuevo mundo";
-products.appendChild(element);
